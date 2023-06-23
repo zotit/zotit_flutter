0d6ffa1d87e7cc10f1ef142242bd070dedba2240
Ramananda Panda <ramanandapanda@hotmail.com>
2023-06-23 10:44:36 +0530

    build fix

--- lib/src/screens/home/home.dart
+++ lib/src/screens/home/home.dart
@@ -73,8 +73,7 @@ class Home extends ConsumerWidget {
   }
 
   _shareNote(String note) async {
-    Share.share("$note \nShared from https://zotit.twobits.in",
-        subject: "note shared from Zotit | Zot anywhere");
+    Share.share("$note \nShared from https://zotit.twobits.in", subject: "note shared from Zotit | Zot anywhere");
   }
 
   _deleteNote(context, ref, id) async {
@@ -163,8 +162,7 @@ class Home extends ConsumerWidget {
     final notesData = ref.watch(noteListProvider);
 
     _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
         ref.read(noteListProvider.notifier).getNotesByPage();
       }
     });
@@ -195,8 +193,7 @@ class Home extends ConsumerWidget {
       body: Center(
         child: Container(
           constraints: const BoxConstraints(maxWidth: 600),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             Container(
               padding: const EdgeInsets.symmetric(
                 vertical: 10,
@@ -225,10 +222,8 @@ class Home extends ConsumerWidget {
                       final _ = ref.refresh(noteListProvider.future);
                     },
                     style: ButtonStyle(
-                        padding: MaterialStateProperty.all(
-                            const EdgeInsets.symmetric(vertical: 20)),
-                        backgroundColor:
-                            MaterialStateProperty.all(const Color(0xFF3A568E))),
+                        padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 20)),
+                        backgroundColor: MaterialStateProperty.all(const Color(0xFF3A568E))),
                     child: const Icon(Icons.done),
                   ),
                 ],
@@ -243,87 +238,70 @@ class Home extends ConsumerWidget {
                       for (int i = 0; i < notes.notes.length; i++)
                         Card(
                           elevation: 8.0,
-                          margin: new EdgeInsets.symmetric(
-                              horizontal: 10.0, vertical: 6.0),
-                          child: Container(
-                            // decoration: const BoxDecoration(color: Color(0xFF3A568E)),
-                            child: ListTile(
-                              contentPadding: const EdgeInsets.symmetric(
-                                  horizontal: 20.0, vertical: 10.0),
-                              title: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      TextButton.icon(
-                                        onPressed: () {
-                                          Clipboard.setData(ClipboardData(
-                                              text: notes.notes[i].text));
-                                        },
-                                        style: ButtonStyle(
-                                          foregroundColor:
-                                              MaterialStateProperty.all<Color>(
-                                                  const Color(0xFF3A568E)),
-                                        ),
-                                        icon: const Icon(Icons.copy),
-                                        label: const Text("Copy"),
+                          margin: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                          child: ListTile(
+                            contentPadding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    TextButton.icon(
+                                      onPressed: () {
+                                        Clipboard.setData(ClipboardData(text: notes.notes[i].text));
+                                      },
+                                      style: ButtonStyle(
+                                        foregroundColor: MaterialStateProperty.all<Color>(const Color(0xFF3A568E)),
                                       ),
-                                      TextButton.icon(
-                                        onPressed: () {
-                                          Navigator.of(context)
-                                              .push(MaterialPageRoute<dynamic>(
-                                            builder: (_) => NoteDetails(
-                                              note: notes.notes[i],
-                                              noteIndex: i,
-                                            ),
-                                          ));
-                                        },
-                                        style: ButtonStyle(
-                                          foregroundColor:
-                                              MaterialStateProperty.all<Color>(
-                                                  const Color(0xFF3A568E)),
-                                        ),
-                                        icon: const Icon(Icons.edit_document),
-                                        label: const Text("Edit"),
-                                      ),
-                                      TextButton.icon(
-                                        onPressed: () {
-                                          _shareNote(notes.notes[i].text);
-                                        },
-                                        style: ButtonStyle(
-                                          foregroundColor:
-                                              MaterialStateProperty.all<Color>(
-                                                  const Color(0xFF3A568E)),
-                                        ),
-                                        icon: const Icon(Icons.share),
-                                        label: const Text("Share"),
-                                      ),
-                                      TextButton.icon(
-                                        onPressed: () async {
-                                          await _deleteNote(
-                                              context, ref, notes.notes[i].id);
-                                        },
-                                        style: ButtonStyle(
-                                          foregroundColor:
-                                              MaterialStateProperty.all<Color>(
-                                            const Color(0xFF3A568E),
+                                      icon: const Icon(Icons.copy),
+                                      label: const Text("Copy"),
+                                    ),
+                                    TextButton.icon(
+                                      onPressed: () {
+                                        Navigator.of(context).push(MaterialPageRoute<dynamic>(
+                                          builder: (_) => NoteDetails(
+                                            note: notes.notes[i],
+                                            noteIndex: i,
                                           ),
-                                        ),
-                                        icon: const Icon(Icons.delete),
-                                        label: const Text("Delete"),
+                                        ));
+                                      },
+                                      style: ButtonStyle(
+                                        foregroundColor: MaterialStateProperty.all<Color>(const Color(0xFF3A568E)),
                                       ),
-                                    ],
-                                  ),
-                                  const Divider(),
-                                  const Gap(5),
-                                  Text(
-                                    notes.notes[i].text,
-                                    style: const TextStyle(
-                                        color: Color(0xFF3A568E),
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
+                                      icon: const Icon(Icons.edit_document),
+                                      label: const Text("Edit"),
+                                    ),
+                                    TextButton.icon(
+                                      onPressed: () {
+                                        _shareNote(notes.notes[i].text.toString());
+                                      },
+                                      style: ButtonStyle(
+                                        foregroundColor: MaterialStateProperty.all<Color>(const Color(0xFF3A568E)),
+                                      ),
+                                      icon: const Icon(Icons.share),
+                                      label: const Text("Share"),
+                                    ),
+                                    TextButton.icon(
+                                      onPressed: () async {
+                                        await _deleteNote(context, ref, notes.notes[i].id);
+                                      },
+                                      style: ButtonStyle(
+                                        foregroundColor: MaterialStateProperty.all<Color>(
+                                          const Color(0xFF3A568E),
+                                        ),
+                                      ),
+                                      icon: const Icon(Icons.delete),
+                                      label: const Text("Delete"),
+                                    ),
+                                  ],
+                                ),
+                                const Divider(),
+                                const Gap(5),
+                                Text(
+                                  notes.notes[i].text,
+                                  style: const TextStyle(color: Color(0xFF3A568E), fontWeight: FontWeight.bold),
+                                ),
+                              ],
                             ),
                           ),
                         )
@@ -333,8 +311,7 @@ class Home extends ConsumerWidget {
                           padding: EdgeInsets.all(50),
                           child: Text(
                             "No Notes Found",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                           ),
                         ),
                       ),
